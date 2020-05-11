b8c5ed911cee7094bb63720d9470d3097e14cba0
Eduard Reger <eduard.reger@arvato.com>
2020-05-11 15:28:26 +0200

    Version bump

---
+++ usefulkit-unity/Assets/UsefulKit/Runtime/GameGlue/Eventsystem/GameEventFloat.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace GameGlue
+{
+    [CreateAssetMenu(fileName = "GameEventFloat", menuName = "GameGlue/GameEventFloat", order = 0)]
+    public class GameEventFloat : GameEventBase<float>
+    {
+
+    }
+}
